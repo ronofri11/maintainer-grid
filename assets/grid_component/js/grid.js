d0d0ae85c5a33b901593f171eff293d6e6ed7808
ronofri11 <renato.onofri@foris.cl>
2015-04-28 18:47:57 -0300

    added parser and full marionette grid version

--- assets/grid_component/js/grid.js
+++ assets/grid_component/js/grid.js
@@ -1,11 +1,112 @@
 define([
     "backbone.marionette",
     "backbone.radio",
-    "radio.shim"
-], function(Marionette, Radio){
+    "radio.shim",
+    "../js/gridparser"
+    // "text!../templates/grid.html"
+], function(Marionette, Radio, Shim, GridParser){
     var Grid = new Marionette.Application();
 
-    Grid.View = new Marionette.CollectionView();
+    Grid.Channel = Radio.channel("grid");
+
+    var Parser = new GridParser();
+    var gridData = Parser.parse("/maintainer-grid/assets/grid/js/json/servicio_jony.json");
+    console.log("gridData:", gridData);
+
+    //set of models and collections needed
+    var Cell = Backbone.Model.extend({});
+
+    var Cells = Backbone.Collection.extend({
+        model: Cell
+    });
+
+    var Column = Backbone.Model.extend({
+    });
+
+    var Columns = Backbone.Collection.extend({
+        model: Column
+    });
+
+    //Views for the Grid App
+    var CellView = Marionette.ItemView.extend({
+        tagName: "li",
+        template: _.template('<span><%-code%></span>'),
+        onRender: function(){
+            var height = this.options.renderParams.height;
+            var start = parseFloat(this.model.get("start"));
+            var end = parseFloat(this.model.get("end"));
+
+            this.$el.css("height", (height * (end - start)) + "px");
+            this.$el.css("top", (height * (start)) + "px");
+            this.$el.css("position", "absolute");
+        }
+    });
+
+    var ColumnView = Marionette.CompositeView.extend({
+        tagName: "ul",
+        className: "col",
+        template: _.template('<h3>column:<%-index%>   </h3>'),
+        childView: CellView,
+
+        initialize: function(options){
+            this.collection = this.model.get("cells");
+            this.childViewOptions = {
+                renderParams: options.renderParams
+            };
+        },
+
+        onRender: function(){
+            this.$el.css("width", this.options.renderParams.width + "%");
+        }
+    });
+
+    var GridView = Marionette.CollectionView.extend({
+        tagName: "div",
+        className: "grid",
+        childView: ColumnView,
+
+        initialize: function(options){
+            this.childViewOptions = {
+                renderParams: options.renderParams
+            };
+        }
+    });
+
+    var GridLayout = Marionette.LayoutView.extend({
+        template: "#grid-layout-template",
+        regions: {
+            grid: ".container"
+        }
+    });
+
+    Grid.on("before:start", function(options){
+        console.log("before:start");
+        //options.columns should contain an array of objects
+        //with a key named cells, and a 
+        Grid.Columns = new Columns(gridData.columns);
+        Grid.Columns.each(function(col){
+            var cells = col.get("cells");
+            var cellCollection = new Cells(cells);
+            col.set("cells", cellCollection);
+        });
+
+        Grid.Layout = new GridLayout();
+    });
+
+    Grid.on("start", function(){
+        console.log("start");
+        //first render is different from the ones to follow
+        Grid.Layout.render();
+        Grid.Layout.on("show", function(){
+            Grid.Layout.getRegion("grid").show(new GridView({
+                collection: Grid.Columns,
+                renderParams: {
+                    height: 410,
+                    width: parseFloat(100.0/Grid.Columns.length)
+                }
+            }));
+        });
+    });
 
     return Grid;
 });
--- js/app.js
+++ js/app.js
@@ -11,6 +11,12 @@ define([
 
     App.Grid = Grid;
 
+    var RootView = Marionette.LayoutView.extend({
+        el: "body"
+    });
+
+    App.Layout = new RootView();
+    App.Layout.addRegion("main", "#somediv");
 
     // var blockparser = new BlockParser();
     // var blocks = blockparser.parse("/maintainers/assets/grid/js/json/servicio_jony.json");
@@ -33,6 +39,56 @@ define([
     // });
 
     console.log(App.Grid);
+
+    console.log("APP GRID IS ABOUT TO START");
+
+    App.Grid.start({columns: 
+        [{
+            index:1,
+            cells:[
+                {
+                  "start": "08:30:00", 
+                  "code": "1.1", 
+                  "end": "09:10:00"
+                }, 
+                {
+                  "start": "09:10:00", 
+                  "code": "1.2", 
+                  "end": "09:50:00"
+                }, 
+                {
+                  "start": "10:00:00", 
+                  "code": "1.3", 
+                  "end": "10:40:00"
+                }
+            ]
+        }, 
+        {
+            index:2,
+            cells:[
+                {
+                  "start": "08:30:00", 
+                  "code": "2.1", 
+                  "end": "09:10:00"
+                }, 
+                {
+                  "start": "09:10:00", 
+                  "code": "2.2", 
+                  "end": "09:50:00"
+                }, 
+                {
+                  "start": "10:00:00", 
+                  "code": "2.3", 
+                  "end": "10:40:00"
+                }
+            ]
+        }
+        ]
+    });
+
+    console.log("APP GRID WAS STARTED");
+
+    App.Layout.getRegion("main").show(App.Grid.Layout);
 
     return App;
 });
